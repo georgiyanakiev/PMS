c1575d5e0ac651590d1c8ac4c82d611e20248833
Georgi Yanakiev <georgiyanakiev@hopsoftware.com>
2019-10-12 22:53:08 +0100

    Add project files.

---
+++ PMS.Entities/Accommodation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Entities
+{
+   public class Accommodation
+    {
+        public int ID { get; set; }
+        public int AccommodationPackageID { get; set; }
+        public AccommodationPackage AccommodationPackage { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
