9fb9901dec7df10657b33fd2680d90da8a6112ae
Georgi Yanakiev <georgiyanakiev@hopsoftware.com>
2019-10-12 23:46:09 +0100

    installed entity framework and enabled migrations

--- PMS.Data/PMSContext.cs
+++ PMS.Data/PMSContext.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using PMS.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PMS.Data
 {
-   public class PMSContext
+    public class PMSContext : DbContext
     {
+        public PMSContext() : base("PMSConnectionString")
+        {
+        }
 
+        public DbSet<AccommodationType> AccommodationTypes { get; set; }
+        public DbSet<AccommodationPackage> AccommodationPackages { get; set; }
+        public DbSet<Accommodation> Accommodations { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
     }
 }
