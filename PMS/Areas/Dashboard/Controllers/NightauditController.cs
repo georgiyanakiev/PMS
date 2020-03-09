2f03c4bd31a1fb40ccab5b115dfb081373fb5b8a
georgi <g.yanakiev@yahoo.com>
2020-03-09 22:46:35 +0000

    added menu options

---
+++ PMS/Areas/Dashboard/Controllers/NightauditController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PMS.Areas.Dashboard.Controllers
+{
+    public class NightauditController : Controller
+    {
+        // GET: Dashboard/Nightaudit
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
