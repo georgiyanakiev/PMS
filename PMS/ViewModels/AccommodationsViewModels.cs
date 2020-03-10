6740d5d3adca82f503bf2d906410cda3cd3ded05
georgi <g.yanakiev@yahoo.com>
2020-03-10 22:31:46 +0000

    added booking detail form

--- PMS/ViewModels/AccommodationsViewModels.cs
+++ PMS/ViewModels/AccommodationsViewModels.cs
@@ -11,9 +11,23 @@ namespace PMS.ViewModels
         public AccommodationType AccommodationType { get; set; }
         public IEnumerable<Accommodation> Accommodations { get; set; }
         public string SearchTerm { get; set; }
-        public int? AccommodationPackageID { get; set; }
+        public int AccommodationPackageID { get; set; }
         public IEnumerable<AccommodationPackage> AccommodationPackages { get; set; }
         public Pager Pager { get; set; }
         public int SelectedAccommodationPackageID { get; set; }
+    }
+    public class AccommodationPackageDetailsViewModel
+    {
+        public AccommodationPackage AccommodationPackage { get; set; }
+    }
+    public class CheckAccommodationAvailabilityViewModel
+    {
+        public DateTime FromDate { get; set; }
+        public int Duration { get; set; }
+        public int NoOfAdults { get; set; }
+        public int NoOfChildren { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Notes { get; set; }
     }
 }
