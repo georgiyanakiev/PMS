df544a318c9635f5f43ff38ab225de12b37acd06
Georgi Yanakiev <g.yanakiev@yahoo.com>
2022-04-23 23:46:56 +0300

    added reservations controller reservations view models updated pms context added entities added services

---
+++ PMS.Services/ReservationTypesService.cs
@@ -0,0 +1,68 @@
+﻿using PMS.Data;
+using PMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Services
+{
+    public class ReservationTypesService
+    {
+        public IEnumerable<ReservationType> GetAllReservationTypes()
+        {
+            var context = new PMSContext();
+
+            return context.ReservationTypes.ToList();
+        }
+
+        public IEnumerable<ReservationType> SearchReservationTypes(string searchTerm)
+        {
+            var context = new PMSContext();
+
+            var reservationTypes = context.ReservationTypes.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                reservationTypes = reservationTypes.Where(a => a.Name.ToLower().Contains(searchTerm.ToLower()));
+            }
+
+            return reservationTypes.ToList();
+        }
+
+        public ReservationType GetReservationTypeByID(int? ID)
+        {
+            var context = new PMSContext();
+
+            return context.ReservationTypes.Find(ID);
+        }
+
+        public bool SaveReservationType(ReservationType reservationType)
+        {
+            var context = new PMSContext();
+
+            context.ReservationTypes.Add(reservationType);
+
+            return context.SaveChanges() > 0;
+        }
+
+        public bool UpdateReservationType(ReservationType reservationType)
+        {
+            var context = new PMSContext();
+
+            context.Entry(reservationType).State = System.Data.Entity.EntityState.Modified;
+
+            return context.SaveChanges() > 0;
+        }
+
+        public bool DeleteReservationType(ReservationType reservationType)
+        {
+            var context = new PMSContext();
+
+            context.Entry(reservationType).State = System.Data.Entity.EntityState.Deleted;
+
+            return context.SaveChanges() > 0;
+        }
+    }
+}
---
+++ PMS/ViewModels/ReservationsViewModels.cs
@@ -0,0 +1,38 @@
+﻿using PMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMS.ViewModels
+{
+    public class ReservationsViewModels
+    {   
+        public ReservationType ReservationType { get; set; }
+        public IEnumerable<Reservation> Reservations { get; set; }
+        public string SearchTerm { get; set; }
+        public int ReservationPackageID { get; set; }
+        public IEnumerable<ReservationPackage> ReservationPackages { get; set; }
+        public Pager Pager { get; set; }
+        public int SelectedReservationPackageID { get; set; }
+    }
+    public class ReservationPackageDetailsViewModel
+    {
+        public ReservationPackage ReservationPackage { get; set; }
+    }
+
+    public class CheckReservationAvailabilityViewModel
+    {
+        public Reservation ReservationsService { get; set; }
+
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public int Duration { get; set; }
+        public int NoOfAdults { get; set; }
+        public int ReservationNumber { get; set; }
+        public int NoOfChildren { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Notes { get; set; }
+    }
+}
